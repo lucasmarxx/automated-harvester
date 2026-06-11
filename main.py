7d9fc1a676a03e95d51f70cc3603bdc66aa847e2
Lucas <lucas.vale.marx@gmail.com>
2026-06-11 16:09:55 -0300

    primeiro commit

--- main.py
+++ main.py
@@ -1,61 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
+# Entrar no site - https://www.olx.com.br
+# Anotar o nome do primeiro produto
+# Anotar o preço do primeiro produto
+# Repetir para todos os produtos da página
+# Guardar informações em arquivo de texto (csv)
 
-url = "http://quotes.toscrape.com"
+from selenium import webdriver
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36' 
-}
+driver = webdriver.Chrome()
 
-resposta = requests.get(url, headers=headers)
-
-if resposta.status_code == 200:
-    print('Conexão deu certo')
-
-else:
-    print(f'falha na conexão. Código: {resposta.status_code}')
-
-print()
-
-if resposta:
-    soup = BeautifulSoup(resposta.text, 'html.parser')
-    quotes = soup.find_all('div', class_='quote')
-
-    for item, quote in enumerate(quotes, 1):
-        texto = quote.find('span', class_='text')
-        autor = quote.find('small', class_='author')
-
-        print(f'{item + 1}. \"{texto}\" - {autor}')
-    
-    dados = []
-
-    for quote in quotes:
-        text = quote.find('span', class_='text').text
-        author = quote.find('small', class_='author').text
-
-        tags = []
-        tags_elementos = quote.find_all('a', class_='tag')
-        for tag in tags_elementos:
-            tags.append(tag.text)
-
-        tags_string = ', '.join(tags)
-
-        dados.append({
-            'Frase': text,
-            'Autor': author,
-            'Tags': tags_string
-        })
-
-    df = pd.DataFrame(dados)
-
-    df.to_csv('Citacoes.csv', index=False, encoding = 'utf-8-sig')
-    # print(f'\nTotal: {len(quotes)} citações encontradas.')
-
-    print(f'{len(dados)} citações salvas em "citacoes.csv"')
-    print('\nPrimeiras 5 citacoes salvas: ')
-    print(df.head())
-
-else:
-    print(f'Erro ao acessar o site. Código: {resposta.status_code}')
-
+driver.get('https://www.olx.com.br')
+input('') # para o site nao fechar imediatamente
