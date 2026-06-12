ca7ff6ebfdd2a56e6600ec0cccba7b3e85e249e7
Lucas <lucas.vale.marx@gmail.com>
2026-06-12 18:30:55 -0300

    Adição de busca de casas em localidade específica

--- main.py
+++ main.py
@@ -5,8 +5,33 @@
 # Guardar informações em arquivo de texto (csv)
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument('--start-maximized')
+
+driver = webdriver.Chrome(options=options)
+
 
 driver.get('https://www.olx.com.br')
+
+
+
+#Montar o XPATH (identificador de elementos)
+
+# tag = input
+# atributo = class 
+# valor = texto laranja
+
+
+pesquisa = driver.find_element(By.XPATH, "//input[@class = 'olx-core-input-textarea-element olx-core-input-element olx-core-input-textarea-element--default']")
+
+if pesquisa:
+    pesquisa.click()
+    pesquisa.send_keys('Aluguel de casas no gama leste')
+    pesquisa.send_keys(Keys.RETURN)
+
 input('') # para o site nao fechar imediatamente
