ae023fe45abb8c66e48de380b0bcc9f917e9683b
Lucas <lucas.vale.marx@gmail.com>
2026-06-15 04:11:33 -0300

    novos refinamentos na busca, adicionada busca por quantidade de quartos

--- main.py
+++ main.py
@@ -15,17 +15,25 @@ driver.get('https://www.olx.com.br/estado-df')
 
 
 
-# Montar o XPATH (identificador de elementos)
+# Montando XPATHs (identificadores de elementos)
 
 pesquisa = driver.find_element(By.XPATH, "//input[@class='olx-core-input-textarea-element olx-core-input-element olx-core-input-textarea-element--default']")
 
 if pesquisa:
     pesquisa.click()
-    pesquisa.send_keys('Aluguel de casas gama')
+    pesquisa.send_keys('Aluguel de casa gama')
     time.sleep(2)
     pesquisa.send_keys(Keys.RETURN)
 
 time.sleep(2)
+
+filtro_aluguel_casa_apto = driver.find_element(By.XPATH, "//p[contains(text(), 'Aluguel')]")
+filtro_aluguel_casa_apto.click()
+
+time.sleep(5)
+
+filtro_quartos = driver.find_element(By.XPATH, "//label[@for = 'chips-id-rooms-3']")
+filtro_quartos.click()
 
 filtro_valor_min = driver.find_element(By.XPATH, "//input[@id = 'price_min']") 
 if filtro_valor_min:
@@ -39,16 +47,8 @@ if filtro_valor_max:
     filtro_valor_max.click()
     filtro_valor_max.send_keys('4000')
 
-time.sleep(2)
-
-botao_pesquisar = driver.find_element(By.XPATH, "//button[@aria-label='aplicar filtro Preço']")
-botao_pesquisar.click()
 
 time.sleep(5)
 
-filtro_casas_e_aptos = driver.find_element(By.XPATH, "//svg[@xmlns='http://www.w3.org/2000/svg']")
-filtro_casas_e_aptos.click()
-
-time.sleep(5)
 
 input('') # para o site nao fechar imediatamente
