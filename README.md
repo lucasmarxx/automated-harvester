a94adc5406706b3603c48ba00a2771dcba26f298
Lucas <lucas.vale.marx@gmail.com>
2026-06-15 03:08:46 -0300

    Mudanças no readme e refinamento na pesquisa

---
+++ README.md
@@ -0,0 +1,17 @@
+<div align='center'>
+
+## Projeto de Automação com Utilização de webscrapper 
+
+</div>
+
+# Objetivo: Realizar busca de casas em região específica na OLX e salvar os dados encontrados.
+
+Entrar no site - https://www.olx.com.br
+
+1. Fazer pesquisa de casas em X localidade
+2. Filtrar valores
+3. Filtrar quantidade de quartos
+4. Realizar a busca em até 3 páginas seguintes
+5. Testar se a resposta das buscas é a correta
+6. Guardar informações em uma base de dados, NOSQL ou csv
+7. 
--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# Entrar no site - https://www.olx.com.br
-# Anotar o nome do primeiro produto
-# Anotar o preço do primeiro produto
-# Repetir para todos os produtos da página
-# Guardar informações em arquivo de texto (csv)
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -12,26 +6,49 @@ import time
 
 options = Options()
 options.add_argument('--start-maximized')
+# options.page_load_strategy = 'eager'
 
 driver = webdriver.Chrome(options=options)
 
 
-driver.get('https://www.olx.com.br')
+driver.get('https://www.olx.com.br/estado-df')
 
 
 
-#Montar o XPATH (identificador de elementos)
+# Montar o XPATH (identificador de elementos)
 
-# tag = input
-# atributo = class 
-# valor = texto laranja
-
-
-pesquisa = driver.find_element(By.XPATH, "//input[@class = 'olx-core-input-textarea-element olx-core-input-element olx-core-input-textarea-element--default']")
+pesquisa = driver.find_element(By.XPATH, "//input[@class='olx-core-input-textarea-element olx-core-input-element olx-core-input-textarea-element--default']")
 
 if pesquisa:
     pesquisa.click()
-    pesquisa.send_keys('Aluguel de casas no gama leste')
+    pesquisa.send_keys('Aluguel de casas gama')
+    time.sleep(2)
     pesquisa.send_keys(Keys.RETURN)
+
+time.sleep(2)
+
+filtro_valor_min = driver.find_element(By.XPATH, "//input[@id = 'price_min']") 
+if filtro_valor_min:
+    filtro_valor_min.click()
+    filtro_valor_min.send_keys('3000')
+
+time.sleep(2)
+
+filtro_valor_max = driver.find_element(By.XPATH, "//input[@id = 'price_max']")
+if filtro_valor_max:
+    filtro_valor_max.click()
+    filtro_valor_max.send_keys('4000')
+
+time.sleep(2)
+
+botao_pesquisar = driver.find_element(By.XPATH, "//button[@aria-label='aplicar filtro Preço']")
+botao_pesquisar.click()
+
+time.sleep(5)
+
+filtro_casas_e_aptos = driver.find_element(By.XPATH, "//svg[@xmlns='http://www.w3.org/2000/svg']")
+filtro_casas_e_aptos.click()
+
+time.sleep(5)
 
 input('') # para o site nao fechar imediatamente
